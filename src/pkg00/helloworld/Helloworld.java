61c7c5e2590cb47b5ea9c2725d17c64c34e77976
danardetienne <danardetienne534@gmail.com>
2026-09-22 15:38:08 +0200

    Update Helloworld.java

--- src/pkg00/helloworld/Helloworld.java
+++ src/pkg00/helloworld/Helloworld.java
@@ -14,7 +14,7 @@ public class Helloworld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.println("Hello World ED");
+       System.out.println("Hello World ED on github");
     }
     
 }
