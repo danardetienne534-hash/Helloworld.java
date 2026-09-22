7f76ad1b635342eaf1616adce33c4bd99fb4a2c0
edanard1 <edanard1@350028R-F115X13.lise.kreizenn.bzh>
2026-09-22 16:37:18 +0200

    exo 2

--- src/pkg00/helloworld/Helloworld.java
+++ src/pkg00/helloworld/Helloworld.java
@@ -14,7 +14,9 @@ public class Helloworld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.println("Hello World ED on github");
+       System.out.println("message ligne1");
+       System.out.println("message ligne2");
+       System.out.println("message ligne3");
     }
     
 }
