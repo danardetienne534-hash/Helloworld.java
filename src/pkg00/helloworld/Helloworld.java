b3d60df4a7999469063c6abeed67ae1893c8bd20
edanard1 <edanard1@350028R-F115X13.lise.kreizenn.bzh>
2026-09-22 17:14:03 +0200

    exo 3

--- src/pkg00/helloworld/Helloworld.java
+++ src/pkg00/helloworld/Helloworld.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package pkg00.helloworld;
-
+ import java.util.Scanner;
 /**
  *
  * @author edanard1
@@ -14,9 +14,29 @@ public class Helloworld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        //Exo2 affichage de message sur plusieurs lignes
+       /**
        System.out.println("message ligne1");
        System.out.println("message ligne2");
        System.out.println("message ligne3");
+       **/
+       //Exo3 
+       /**
+       Scanner sc = new Scanner(System.in);
+        int monAge = 0;
+
+
+        System.out.print("Quel est votre age ? : ");
+        monAge = sc.nextInt();
+
+
+        System.out.println("Vous avez " + monAge + " an(s)");
+       **/
+       
+       //Ex4
+       int x = 10,y = 20 , temp
+       
     }
-    
-}
+}   
+
