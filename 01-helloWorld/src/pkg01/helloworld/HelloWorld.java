b08757aceb3297d459f15a57a93e12a5e46adf84
edanard1 <edanard1@350028R-F115X09.lise.kreizenn.bzh>
2026-09-25 11:20:43 +0200

    version 1.2

---
+++ 01-helloWorld/src/pkg01/helloworld/HelloWorld.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg00.helloworld;
+ import java.util.Scanner;
+/**
+ *
+ * @author edanard1
+ */
+public class Helloworld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //Exo2 affichage de message sur plusieurs lignes
+       /**
+       System.out.println("message ligne1");
+       System.out.println("message ligne2");
+       System.out.println("message ligne3");
+       **/
+       //Exo3 
+       /**
+       Scanner sc = new Scanner(System.in);
+        int monAge = 0;
+
+
+        System.out.print("Quel est votre age ? : ");
+        monAge = sc.nextInt();
+
+
+        System.out.println("Vous avez " + monAge + " an(s)");
+       **/
+       
+       //Ex4
+       Scanner sc = new Scanner(System.in);
+       
+       /** int x = 0;
+       int y = 0;
+       int z = 0;
+       System.out.println("x:");
+       x = sc.nextInt();
+       System.out.println("y:");
+       y = sc.nextInt();
+       System.out.println("x:" + x + "------y:" + y);
+       z = x;
+       x = y;
+       y = z;
+       System.out.println("x:" + x + "------y:" + y);
+       **/
+       
+               
+       
+    }
+}   
